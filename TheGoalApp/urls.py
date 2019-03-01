04e470cc9ad87ba0056d451ffa1d17c8c239512c
Austin Brown <austinmbrown22@gmail.com>
2019-03-01 12:54:33 -0600

    Created singup placeholder

--- TheGoalApp/urls.py
+++ TheGoalApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='singup'),
 ]
--- TheGoalApp/views.py
+++ TheGoalApp/views.py
@@ -5,3 +5,5 @@ def home(request):
     return render(request, 'TheGoalApp/theGoalMainPg.html')
 def login(request):
     return render(request, 'TheGoalApp/logInPg.html')
+def signup(request):
+    return render(request, 'TheGoalApp/SignUpPg.html')
