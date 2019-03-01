6c4a4fdbe9acfaba771cde82f1e7361ba6163476
Daniel Aguila <danielbaguila@gmail.com>
2019-02-28 18:14:25 -0600

    LogInPg

--- TheGoalApp/views.py
+++ TheGoalApp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'TheGoalApp/theGoalMainPg.html')
+def profile(request):
+    return render(request, 'TheGoalApp/logInPg.html')
