f48371474cdf2326c837072786fb7025710c8811
Daniel Aguila <danielbaguila@gmail.com>
2019-03-01 12:38:15 -0600

    profile page

--- TheGoalApp/views.py
+++ TheGoalApp/views.py
@@ -5,3 +5,5 @@ def home(request):
     return render(request, 'TheGoalApp/theGoalMainPg.html')
 def login(request):
     return render(request, 'TheGoalApp/logInPg.html')
+def profile(request):
+    return render(request, 'TheGoalApp/profile.html')
