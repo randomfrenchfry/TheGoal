a79a1015b8c762ea47b34ce3884d6cd283ec105d
Austin Brown <austinmbrown22@gmail.com>
2019-03-03 17:34:01 -0600

    syncing local and remote

--- TheGoalApp/urls.py
+++ TheGoalApp/urls.py
@@ -5,9 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
-<<<<<<< HEAD
     path('signup/', views.signup, name='singup'),
-=======
     path('profile/', views.profile, name='profile')
->>>>>>> 3cdd1154f54887111a427815c9cbe9e97d8f3240
 ]
